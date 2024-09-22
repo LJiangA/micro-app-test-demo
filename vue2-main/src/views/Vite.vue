61716da78b56f970dcfbb4324185481263d76fa3
zjy <zhengjyi1@163.com>
2024-09-22 22:59:34 +0800

    Initial commit

---
+++ vue2-main/src/views/Vite.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="vite">
+    <micro-app
+      :name="name"
+      :url="url"
+      iframe
+    ></micro-app>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'vite-sub-app',
+  data () {
+    return {
+      name: 'vite-sub-app',
+      url: 'http://localhost:5173/'
+    }
+  }
+}
+</script>
